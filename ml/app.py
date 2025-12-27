30ee34bff76b636dfb049301338a807342a80a44
Pradyumn Agrahari <pradyumnagrahari111@gmail.com>
2025-12-27 20:30:12 +0530

    issue fixed

--- ml/app.py
+++ ml/app.py
@@ -59,6 +59,5 @@ def generate_week():
     return jsonify(plan)
 
 if __name__ == "__main__":
-    if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
